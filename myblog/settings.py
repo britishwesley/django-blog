144725a2ebc8aae4b114909cb70de1847e540d1d
Wesley Dodo <britishwesley332@gmail.com>
2026-08-01 13:30:59 +0100

    update setting.py

--- myblog/settings.py
+++ myblog/settings.py
@@ -50,7 +50,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'whitenoise.middleware.whitenoisemiddleware'
+    'whitenoise.middleware.Whitenoisemiddleware'
 ]
 
 ROOT_URLCONF = 'myblog.urls'
