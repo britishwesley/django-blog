310a2a9fb6b57ed3ac4bf69b4eaf7f26a023fc67
Wesley Dodo <britishwesley332@gmail.com>
2026-08-05 14:22:56 +0100

    updated on my profile

--- myblog/settings.py
+++ myblog/settings.py
@@ -31,7 +31,7 @@ DATABASE_URL = os.getenv("DATABASE")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['django-blog-wbiv.onrender.com']
+ALLOWED_HOSTS = ['127.0.0.1:8000', 'localhost', 'django-blog-wbiv.onrender.com']
 
 
 # Application definition
@@ -81,18 +81,21 @@ WSGI_APPLICATION = 'myblog.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': dj_database_url.config(conn_max_age=600,
+#                                      default=DATABASE_URL
+
+#                                     )
+# }
+
 DATABASES = {
-    'default': dj_database_url.config(conn_max_age=600,
-                                     default=DATABASE_URL
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
 
-                                    )
-
-        # 'ENGINE': 'django.db.backends.sqlite3',
-        # 'NAME': BASE_DIR / 'db.sqlite3',
-    
 }
 
-DATABASES['default'] = dj_database_url.config(conn_max_age=600)
 
 # Password validation
 # https://docs.djangoproject.com/en/6.0/ref/settings/#auth-password-validators
